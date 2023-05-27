511f6d607a2f5deaa31fd9973aa924e6ccea8d53
Rumen Mihaylov <rumen.mihaylov@ship.cars>
2023-05-27 06:52:34 +0300

    Initial commit

--- mpttune/model/__init__.py
+++ mpttune/model/__init__.py
@@ -1,6 +1,24 @@
 from mpttune.model.mpt.config import (
     MPT7B8bitConfig, MPT7BChat8bitConfig, MPT7BInstruct8bitConfig, MPT7BStorywriter4BitConfig, MPT7BStorywriter8bitConfig)
 
+from mpttune.model.llama.config import (
+    LLama7B4BitConfig, LLama13B4BitConfig, LLama30B4BitConfig, LLama65B4BitConfig)
+
+
+MPT_CONFIGS = {
+    MPT7B8bitConfig.name: MPT7B8bitConfig,
+    MPT7BChat8bitConfig.name: MPT7BChat8bitConfig,
+    MPT7BInstruct8bitConfig.name: MPT7BInstruct8bitConfig,
+    MPT7BStorywriter8bitConfig.name: MPT7BStorywriter8bitConfig,
+    MPT7BStorywriter4BitConfig.name: MPT7BStorywriter4BitConfig,
+}
+
+LLAMA_CONFIGS = {
+    LLama7B4BitConfig.name: LLama7B4BitConfig,
+    LLama13B4BitConfig.name: LLama13B4BitConfig,
+    LLama30B4BitConfig.name: LLama30B4BitConfig,
+    LLama65B4BitConfig.name: LLama65B4BitConfig,
+}
 
 MODEL_CONFIGS = {
     MPT7B8bitConfig.name: MPT7B8bitConfig,
@@ -8,6 +26,11 @@ MODEL_CONFIGS = {
     MPT7BInstruct8bitConfig.name: MPT7BInstruct8bitConfig,
     MPT7BStorywriter8bitConfig.name: MPT7BStorywriter8bitConfig,
     MPT7BStorywriter4BitConfig.name: MPT7BStorywriter4BitConfig,
+
+    LLama7B4BitConfig.name: LLama7B4BitConfig,
+    LLama13B4BitConfig.name: LLama13B4BitConfig,
+    LLama30B4BitConfig.name: LLama30B4BitConfig,
+    LLama65B4BitConfig.name: LLama65B4BitConfig,
 }
 
 
@@ -17,8 +40,12 @@ def load_model(model_name: str, weights, half=False, backend='triton'):
 
     model_config = MODEL_CONFIGS[model_name]
 
-    if model_name in MODEL_CONFIGS:
+    if model_name in MPT_CONFIGS:
         from mpttune.model.mpt.model import load_model
+        model, tokenizer = load_model(model_config, weights, half=half, backend=backend)
+
+    elif model_name in LLAMA_CONFIGS:
+        from mpttune.model.llama.model import load_model
         model, tokenizer = load_model(model_config, weights, half=half, backend=backend)
 
     else:
---
+++ mpttune/model/llama/config.py
@@ -0,0 +1,42 @@
+class LLama7B4BitConfig:
+    name = 'llama-7b-4bit'
+    hf_config_name = "decapoda-research/llama-7b-hf"
+    hf_tokenizer_config = "huggyllama/llama-13b"
+    bits = 4
+    groupsize = 128
+    max_seq_len = 2048
+    attn_impl = 'torch'
+    device_map = "auto"
+
+
+class LLama13B4BitConfig:
+    name = 'llama-13b-4bit'
+    hf_config_name = "decapoda-research/llama-13b-hf"
+    hf_tokenizer_config = "huggyllama/llama-13b"
+    bits = 4
+    groupsize = 128
+    max_seq_len = 2048
+    attn_impl = 'torch'
+    device_map = "auto"
+
+
+class LLama30B4BitConfig:
+    name = 'llama-30b-4bit'
+    hf_config_name = "decapoda-research/llama-30b-hf"
+    hf_tokenizer_config = "huggyllama/llama-13b"
+    bits = 4
+    groupsize = 128
+    max_seq_len = 2048
+    attn_impl = 'torch'
+    device_map = "auto"
+
+
+class LLama65B4BitConfig:
+    name = 'llama-65b-4bit'
+    hf_config_name = "decapoda-research/llama-65b-hf"
+    hf_tokenizer_config = "huggyllama/llama-13b"
+    bits = 4
+    groupsize = 128
+    max_seq_len = 2048
+    attn_impl = 'torch'
+    device_map = "auto"
